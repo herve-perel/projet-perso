1031938f661c9fe84439b38c164726d9767409d2
herve <herveperel@gmail.com>
2023-06-03 08:00:45 +0200

    update gitignore

---
+++ src/Controller/FilmController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Film;
+use App\Form\FilmType;
+use App\Repository\FilmRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/film', name: 'film_')]
+class FilmController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(FilmRepository $filmRepository): Response
+    {
+        $films = $filmRepository->findAll();
+        return $this->render('film/index.html.twig', [
+            'films' => $films,
+        ]);
+    }
+
+    #[Route('/new', name: 'new')]
+    public function new(Request $request, FilmRepository $filmRepository): Response
+    {
+        $film = new Film();
+        $form = $this->createForm(FilmType::class, $film);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $filmRepository->save($film, true);
+
+            return $this->redirectToRoute('film_index');
+        }
+
+        return $this->render('film/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', methods: ['GET'], name: 'show')]
+    public function show(int $id, FilmRepository $filmRepository): Response
+    {
+        $film = $filmRepository->findOneBy(['id' => $id]);
+        if (!$film) {
+            throw $this->createNotFoundException(
+                'No film with id : ' . $id . ' found in film\'s table.'
+            );
+        }
+
+        return $this->render('film/show.html.twig', [
+
+            'film' => $film,
+
+        ]);
+    }
+}
---
+++ src/Form/FilmType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Film;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
+class FilmType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('category', TextType::class)
+            ->add('year', NumberType::class)
+            ->add('synopsis', TextType::class)
+            ->add('poster', TextType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Film::class,
+        ]);
+    }
+}
