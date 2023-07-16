e161822f267011b87bc7a399a73837a97d9edd5b
herve <herveperel@gmail.com>
2023-07-16 17:21:12 +0200

    search bar

--- src/Form/SearchFilmType.php
+++ src/Form/SearchFilmType.php
@@ -2,27 +2,29 @@
 
 namespace App\Form;
 
-use App\Entity\Support;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
 
-class SupportType extends AbstractType
+class SearchFilmType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('format', TextType::class, [
-                'placeholder' => 'Choississez un format'
-            ])
-        ;
+            ->setMethod('GET')
+            ->add('search', SearchType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Support::class,
+            'csrf_protection' => false,
         ]);
+    }
+
+    public function getBlockPrefix()
+    {
+        return '';
     }
 }
